6326123cc547083091e81d3c7d4ce501ee4d5942
Han-yushui <2068325343@qq.com>
2024-09-19 22:02:40 +0800

    Update MyStarterTemplate.java

--- MyStarterTemplate.java
+++ MyStarterTemplate.java
@@ -8,6 +8,7 @@ public class MyStarterTemplate {
         this.myStarterProperties = myStarterProperties;
     }
 
+    // 方法的注释
     public void printName() {
         System.out.println("获取myStarterProperties的名字");
         System.out.println("myStarterProperties.getName() = " + myStarterProperties.getName());
