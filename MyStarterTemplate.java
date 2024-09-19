0decc8b04472b391e99b76d5dcd87ec66a4b58cb
zhanghuihui <2068325343@qq.com>
2024-09-19 22:32:33 +0800

    update

--- MyStarterTemplate.java
+++ MyStarterTemplate.java
@@ -3,14 +3,13 @@ package org.example.mystarter.my;
 public class MyStarterTemplate {
 
     MyStarterProperties myStarterProperties;
-
+    
     public MyStarterTemplate(MyStarterProperties myStarterProperties) {
         this.myStarterProperties = myStarterProperties;
     }
 
     // 方法的注释
     public void printName() {
-        System.out.println("获取myStarterProperties的名字");
         System.out.println("myStarterProperties.getName() = " + myStarterProperties.getName());
     }
 }
